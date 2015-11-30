0e496df23124da345425d03ec6c9ebb956f458e6
William Chiu <williamchiu@Williams-iMac.local>
2015-11-30 01:25:20 -0500

    fix abs path

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='trinity',
-    version='0.5',
+    version='0.6',
     description='Prints out assignments from trinityschoolnyc.org',
     url='https://github.com/WilliamChiu/trinity',
     author='William Chiu',
