b9354c5bd132fe6ab84227addcefa7da4927612e
William Chiu <williamchiu@Williams-iMac.local>
2015-11-30 17:56:28 -0500

    testing zips. using semantic versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='trinity',
-    version='0.62',
+    version='0.6.0-beta.1',
     description='Prints out assignments from trinityschoolnyc.org',
     url='https://github.com/WilliamChiu/trinity',
     author='William Chiu',
