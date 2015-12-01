ca788b7e7e0931dd131745e9780bf1719e7d6b5f
William Chiu <williamchiu@Williams-iMac.local>
2015-11-30 19:31:42 -0500

    testing upgrade

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='trinity',
-    version='0.1.0',
+    version='0.2.0',
     description='Prints out assignments from trinityschoolnyc.org',
     url='https://github.com/WilliamChiu/trinity',
     author='William Chiu',
